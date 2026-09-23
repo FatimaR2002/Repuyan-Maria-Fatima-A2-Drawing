35095853b872f8df7d63e63b36141b50567c1706
fatima <000991940@mohawkcollege.ca>
2026-09-23 18:21:44 -0400

    pivoted to change the rectangle "Stem", to a capsule, to angle the stem

--- GAME 10033 Game Development Foundations - 2D Game Template (v1.6)1/Game.cs
+++ GAME 10033 Game Development Foundations - 2D Game Template (v1.6)1/Game.cs
@@ -29,37 +29,37 @@ namespace MohawkGame2D
             //Make the background a light blue colour
             Window.ClearBackground(135, 206, 235);
 
-            //Draw a rectangle at upper left corner position and make the colour a light brown colour
+            //Draw a capsule for the stem and make the colour green
             Draw.SetLineColor(0,0,0);
             Draw.SetLineSize(1);
             Draw.SetFillColor(138, 154, 91);
-            Draw.Rectangle(200, 200, 10, 200);
+            Draw.Capsule(200, 200,80,400, 6);
+
+            //Drawing the flower pot
+
+            //Drawing first layer of petals
+            //First petal
+            Draw.SetLineColor(0, 0, 0);
+            Draw.SetLineSize(1);
+            Draw.SetFillColor(255, 255, 240);
+            Draw.Capsule(200, 200, 200, 360, 10);
+            //Second petal
+            //Draw.SetLineColor(0, 0, 0);
+            //Draw.SetLineSize(1);
+            //Draw.SetFillColor(255, 255, 240);
+            //Draw.Capsule(200, 200, 280, 360, 10);
+            ////Third petal
+            //Draw.SetLineColor(0, 0, 0);
+            //Draw.SetLineSize(1);
+            //Draw.SetFillColor(255, 255, 240);
+            //Draw.Capsule(200, 200, 270, 360, 10);
+
 
             //Draw a circle at the top of the rectangle and make the colour orange
             Draw.SetLineColor(255, 117, 24);
             Draw.SetLineSize(1);
             Draw.SetFillColor(254, 216, 94);
             Draw.Circle(200, 200, 30);
-
-
-            //Draw the 4 pinwheel triangles and make them follow the mouse cursor position, from the center of the square
-            //The First Pinwheel triangle is coloured green
-            //Draw.SetLineColor(79, 121, 66);
-            //Draw.SetLineSize(1);
-            //Draw.SetFillColor(0, 158, 96);
-            //Draw.Triangle(200, 200, 340, 360, 220,340);
-
-            //The Second Pinwheel triangle is coloured yellow
-            Draw.SetLineColor(255, 170, 51);
-            Draw.SetLineSize(1);
-            Draw.SetFillColor(255, 192, 0);
-            Draw.Triangle(200, 200, 360, 60, 340, 180);
-
-            //The Third Pinwheel triangle is coloured red
-          
-
-
-
         }
     }
 
