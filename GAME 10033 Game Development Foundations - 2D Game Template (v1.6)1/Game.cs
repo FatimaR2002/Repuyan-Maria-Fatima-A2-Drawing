012aa94d68a078ed6ddc5cf6ef1b12ecbc727763
fatima <000991940@mohawkcollege.ca>
2026-09-23 13:24:12 -0400

    added an outline for the square and rectangle

--- GAME 10033 Game Development Foundations - 2D Game Template (v1.6)1/Game.cs
+++ GAME 10033 Game Development Foundations - 2D Game Template (v1.6)1/Game.cs
@@ -30,11 +30,14 @@ namespace MohawkGame2D
             Window.ClearBackground(135, 206, 235);
 
             //Draw a rectangle at upper left corner position and make the colour a light brown colour
-            Draw.SetLineSize(0);
+            Draw.SetLineColor(204, 119, 34);
+            Draw.SetLineSize(1);
             Draw.SetFillColor(218, 160, 109);
             Draw.Rectangle(180, 200, 20, 200);
 
             //Draw a square at the top of rectangle and make it orange
+            Draw.SetLineColor(255, 117, 24);
+            Draw.SetLineSize(1);
             Draw.SetFillColor(255, 165, 0);
             Draw.Rectangle(160, 140, 60, 60);
 
