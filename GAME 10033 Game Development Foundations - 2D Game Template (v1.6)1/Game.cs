2f024196ef8aa42cbb733ccccc06cf49013f64e1
fatima <000991940@mohawkcollege.ca>
2026-09-23 03:48:38 -0400

    Added a big circle and a tiny circle

--- GAME 10033 Game Development Foundations - 2D Game Template (v1.6)1/Game.cs
+++ GAME 10033 Game Development Foundations - 2D Game Template (v1.6)1/Game.cs
@@ -33,6 +33,15 @@ namespace MohawkGame2D
             Draw.SetLineSize(0);
             Draw.SetFillColor(218, 160, 109);
             Draw.Rectangle(180, 200, 20, 200);
+
+            //Draw a circle at the top of rectangle and make it orange
+            Draw.SetFillColor(248, 131, 121);
+            Draw.Circle(190, 190, 50);
+
+            //Draw a tiny circle at the center of the big circle and make it black
+            Draw.SetFillColor(0, 0, 0);
+            Draw.Circle(190, 190, 8);
+
         }
     }
 
