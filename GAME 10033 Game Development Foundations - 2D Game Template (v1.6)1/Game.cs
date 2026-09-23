57a9910fd34cc974357b1f941807c2656f5d5663
fatima <000991940@mohawkcollege.ca>
2026-09-23 02:50:16 -0400

    Added the window title (PinWheel Spinner), window size (400x400 px) and made the background a light blue colour

---
+++ GAME 10033 Game Development Foundations - 2D Game Template (v1.6)1/Game.cs
@@ -0,0 +1,35 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+            //Set the window title
+            Window.SetTitle("PinWheel Spinner");
+            //Set the window size to 400x400 pixels
+            Window.SetSize(400, 400);
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+            //Make the background a light blue colour
+            Window.ClearBackground(240, 255, 255);
+
+        }
+    }
+
+}
